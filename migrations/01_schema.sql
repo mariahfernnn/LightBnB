f9cb251a131d5fe3b7d053b7b995bd857555e419
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-16 16:20:51 +0000

    LighthouseBnB DDL Challenge - create table statements and drop table statements for LighthouseBnB

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,5 +1,62 @@
+--Include DROP TABLE statements to rerun the file easily
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
 --CREATE TABLE statements for LightBnB
 CREATE TABLE users (
-
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
 );
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  title TEXT NOT NULL,
+  description TEXT,
+  thumbnail_photo_url TEXT NOT NULL,
+  cover_photo_url TEXT NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+
+  country TEXT NOT NULL,
+  street TEXT NOT NULL,
+  city TEXT NOT NULL,
+  province TEXT NOT NULL,
+  post_code TEXT NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT true
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night INTEGER,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
+
 
