c4354b1e802b0a2351e78126aaa56801e3cbfaa7
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-17 22:42:46 +0000

    LighthouseBnB Add Property Challenge - Updated addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -132,7 +132,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties 
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   let check = false;
@@ -193,10 +193,54 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
+ //
+ // Assisted by Andrew Matte - make sure parameters you are passing in the INSERT TO statements are exactly the same as the values
+ // Assisted by Will Hawkins - make sure you are adding new listings into the database - added owner_id
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+  INSERT INTO properties (
+    owner_id,
+    title,
+    description, 
+    cover_photo_url, 
+    thumbnail_photo_url, 
+    cost_per_night, 
+    parking_spaces, 
+    number_of_bathrooms, 
+    number_of_bedrooms, 
+    province, 
+    city, 
+    country, 
+    street, 
+    post_code) 
+    VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+
+  const values = [property.owner_id, property.title, 
+    property.description, 
+    property.cover_photo_url, 
+    property.thumbnail_photo_url, 
+    property.cost_per_night,
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms, 
+    property.province, 
+    property.city, 
+    property.country,
+    property.street, 
+    property.post_code, 
+    ];
+
+    return pool.query(queryString, values)
+  .then(property => property.rows[0])
+  .catch(err => console.error(err));
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
