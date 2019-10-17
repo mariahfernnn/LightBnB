fd5110e58e7314d81eadeeca5daf271aed9af9fb
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-17 13:49:38 +0000

    Use node-postgres to have the getAllProperties query the lightbnb database and return res.rows as the result of a promise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -77,15 +77,18 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
+
+ // Use node-postgres to have the getAllProperties query the lightbnb database
+ // Return res.rows as the result of a promise
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * 
+  FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
